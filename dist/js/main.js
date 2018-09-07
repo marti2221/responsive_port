2d0a34950421bf632b005606ae55398d5db7fffb
Travis Martin <martiwebdev@gmail.com>
2018-09-07 14:23:18 -0400

    initial redo

--- dist/js/main.js
+++ dist/js/main.js
@@ -3,7 +3,7 @@ const menuBtn = document.querySelector(".menu-btn");
 const menu = document.querySelector(".menu");
 const menuNav = document.querySelector(".menu-nav");
 const menuBranding = document.querySelector(".menu-branding");
-const navItems = document.querySelectorAll(".nav-item");
+const navItems = document.querySelectorAll(".menu-nav-item");
 
 // set state of menu
 let showMenu = false; 
@@ -32,4 +32,67 @@ function toggleMenu() {
         // set state
         showMenu = false; 
     }
+}
+
+
+
+
+
+
+function initMap() {
+	/*
+	let styledMapType = new google.maps.styledMapTypes([
+		{elementType: "geometry", stylers: [{color: "rgb(20,22,55)"}]},
+		{elementType: "labels.text.fill", stylers: [{color: "rgb(47, 102, 218)"}]},
+		{elementType: "labels.text.stroke", stylers: [{color: "rgb(255,80,80)"}]},
+		{
+			featureType: "administrative",
+			elementType: "geometry.stroke",
+			stylers: [{color: "grey"}]
+		},
+		{
+			featureType: "administrative.land_parcel",
+			elementType: "geometry.stroke",
+			stylers: [{color: "rgb(20,22,55)"}]
+		},
+		{
+			featureType: "administrative.land_parcel",
+			elementType: "geometry.text.fill",
+			stylers: [{color: "rgb(47,102,218)"}]
+		},
+		{
+			featureType: "landscape.natural",
+			elementType: "geometry",
+			stylers: [{color: "darkgrey"}]
+		},
+		{
+			featureType: "poi",
+			elementType: "geometry",
+			stylers: [{color: "darkgrey"}]
+		},
+		{
+			featureType: "poi",
+			elementType: "labels.text.fill",
+			stylers: [{color: "rgb(20,22,55)"}]
+		},
+		{
+			featureType: "poi.park",
+			elementType: "geometry.fill",
+			stylers: [{color: "grey"}]
+		},
+		{
+			featureType: "poi.park",
+			elementType: "labels.text.fill",
+			stylers: [{color: "rgb(47,102,218)"}]
+		}
+	], {})
+	*/
+    let mapDiv = document.getElementById("map");
+    let map = new google.maps.Map(mapDiv, {
+        center: {
+            lat: 43.416041,
+            lng: -83.978485
+        },
+        zoom: 10
+    });
 }
